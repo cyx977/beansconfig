a71a497e79c3d45b1e68b2e7bf10753ab735d98e
cyx977 <cyx977@outlook.com>
2021-12-12 20:39:02 +0545

    log4j

--- hibernate-tutorial/src/com/hibtut/app/TestJdbc.java
+++ hibernate-tutorial/src/com/hibtut/app/TestJdbc.java
@@ -23,25 +23,29 @@ public class TestJdbc {
 			SessionFactory factory = new Configuration().configure("cfg.xml").addAnnotatedClass(Student.class).buildSessionFactory();
 //			Session session = factory.getCurrentSession();	
 			final Session session = factory.openSession();
-//			Student s = new Student();
-//			s.setName("whatisiasdfasdft");
+			Student s = new Student();
+			s.setName("random guy");
 //			s.setId(2);
 			session.beginTransaction();
-//			int theId = (Integer) session.save(s);
-//			System.out.println(theId);
+			int theId = (Integer) session.save(s);
+			System.out.println(theId);
 //			Student s1 = session.get(Student.class, 1);
 //			System.out.println(s1.getName());
 //			Thread.sleep(1000*50);
-//			System.out.println("done sleeping");
-			List<Student> students = session.createQuery("from Student s where s.id='2'").getResultList();
-			for(Student s : students) {
-				System.out.println(s);
-			}
+//			System.out.println("done sleeping");https://img-c.udemycdn.com/redactor/2016-10-06_12-44-20-3b68464e433153ad29cb254bb1f7f136/image4.png
+//			List<Student> students = session.createQuery("from Student s where s.id='2' OR s.id='3'").getResultList();
+//			displayStudents(students);
 			
 			session.getTransaction().commit();
 			factory.close();
 		}catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+
+	private static void displayStudents(List<Student> students) {
+		for(Student s : students) {
+			System.out.println(s);
 		}
 	}
 
