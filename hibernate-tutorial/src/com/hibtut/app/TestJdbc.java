2723cd7ca93bd8c9fe64d3ee4750b664f181d95a
cyx977 <cyx977@outlook.com>
2021-12-13 08:28:29 +0545

    udpate and delete

--- hibernate-tutorial/src/com/hibtut/app/TestJdbc.java
+++ hibernate-tutorial/src/com/hibtut/app/TestJdbc.java
@@ -23,18 +23,32 @@ public class TestJdbc {
 			SessionFactory factory = new Configuration().configure("cfg.xml").addAnnotatedClass(Student.class).buildSessionFactory();
 //			Session session = factory.getCurrentSession();	
 			final Session session = factory.openSession();
-			Student s = new Student();
-			s.setName("random guy");
+//			Student s = new Student();
+//			s.setName("random guy");
 //			s.setId(2);
 			session.beginTransaction();
-			int theId = (Integer) session.save(s);
-			System.out.println(theId);
+//			int theId = (Integer) session.save(s);
+//			System.out.println(theId);
 //			Student s1 = session.get(Student.class, 1);
 //			System.out.println(s1.getName());
 //			Thread.sleep(1000*50);
 //			System.out.println("done sleeping");https://img-c.udemycdn.com/redactor/2016-10-06_12-44-20-3b68464e433153ad29cb254bb1f7f136/image4.png
 //			List<Student> students = session.createQuery("from Student s where s.id='2' OR s.id='3'").getResultList();
 //			displayStudents(students);
+			
+//			Student s = session.get(Student.class, 1);
+//			s.setName("updated Name");
+			
+			
+			//update all names
+//			session.createQuery("update Student set name='j sukai'").executeUpdate();
+			
+			//delete a student
+//			int theId = 1;
+//			Student s = session.get(Student.class, theId);
+//			session.delete(s);
+			
+			
 			
 			session.getTransaction().commit();
 			factory.close();
